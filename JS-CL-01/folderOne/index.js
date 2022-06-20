b75a100d52949481a495606362f0cdd630f4ddf3
olakunlebalogun <olakunlebalogun247@gmail.com>
2022-06-20 13:01:13 +0100

    some little amendments

--- JS-CL-01/folderOne/index.js
+++ JS-CL-01/folderOne/index.js
@@ -40,7 +40,7 @@ let width = 34.89;
 let teacher = "Mr John"
 
 // job = 25;
-console.log(job);
+// console.log(job);
 function printHello() {
     
     console.log("This is just function")
@@ -68,7 +68,7 @@ const laptop = {
     width : 300,
 }
 
-console.log(laptop['color'])
+// console.log(laptop['color'])
 
 
 
@@ -79,15 +79,15 @@ const myMun = "ruth"
 
 const newAge = 56
 const strAge = newAge.toString()
-console.log(newAge, strAge);
+// console.log(newAge, strAge);
 
 
 const stringNumber = "3445s"
 const integerNumber = Number(stringNumber);
 
 
-console.log(stringNumber, integerNumber);
-console.log(-5/0);
+// console.log(stringNumber, integerNumber);
+// console.log(-5/0);
 
 const ageA = "";
 const ageB = 23;
@@ -95,14 +95,14 @@ const ageC = 18;
 
 
 const ageABC = ageB + ageA + ageC;
-console.log(ageABC);
+// console.log(ageABC);
 
 
 const anthem1 = "arise o' compatriot \n";
 const anthem2 = "Nigeria's call obey \n";
 const anthem3 = "To serve our fatherland \n"
 const anthem = anthem1 + " " + anthem2 + " " + anthem3;
-console.log(anthem)
+// console.log(anthem)
 
 // const newAnthem = "Arise o' compatriot" + "Nigeria's call obey" + "To serve our fatherland"
 // /n  === newline;
@@ -114,14 +114,14 @@ const nBoys = 80;
 const nGirls = 15;
 
 const statementA = "there are " + nBoys + " boys and " + nGirls + " girls in this class";
-console.log(statementA);
+// console.log(statementA);
 
 const statementB = `there are ${nBoys} boys and ${nGirls} girls in this class`
 
-console.log(statementB);
+// console.log(statementB);
 
 const statementC = `${23 +99}`
-console.log(statementC);
+// console.log(statementC);
 
 
 // Operators Operands
@@ -131,11 +131,58 @@ const subNum = nGirls - nBoys;
 const multNum = nBoys * nGirls;
 const divNum = nBoys / nGirls;
 const remNum = nBoys % nGirls;
-console.log(addNum, subNum, multNum, divNum, remNum)
+// console.log(addNum, subNum, multNum, divNum, remNum)
 
 let size = 12;
 // console.log(++size)
 
 // size = size + 5
 size %= 5;
-console.log(size)
+// console.log(size)
+
+/**
+ * Arrays in JS
+ */
+
+const bibleNames = ["matthew", "mark", "luke", "john", 6, true];
+// console.log(bibleNames.length)
+// console.log(bibleNames.push("acts"))
+// console.log(bibleNames.push(45))
+// console.log(bibleNames.pop())
+// console.log(bibleNames.shift())
+// console.log(bibleNames.unshift("malachi"))
+
+const joinedNames = bibleNames.join(",!")
+console.log(joinedNames)
+
+const myOnlyName = "Olakunle Balogun Olaoluwa";
+console.log(myOnlyName.split("l"))
+// ["O", "akun","e Ba", "ogun", "O", "ao" ,"uwa"]
+// ["Ol", "kunle B", "logun Ol", "oluw", ""]
+
+const realBibleNames = bibleNames.slice(0,4)
+console.log(bibleNames.indexOf("mark"))
+
+const mosesBooks = ["Genesis", "Exodus", "Leviticus", "Number", "Deuteronomy"]
+const booksOfProphets = ["Judges", "Kings 1", "Kings 2", "Joshua"];
+
+const oldTestaments = mosesBooks.concat(booksOfProphets)
+console.log(mosesBooks)
+console.log(oldTestaments)
+
+
+console.log(realBibleNames)
+console.log(bibleNames)
+
+
+/***
+ * 
+ * 1. if and esle,
+ * 2. loops
+ * 3. conditions,
+ * 4. falsy and truthy values
+ * 5. ternary
+ * 6. swicth statement
+ * 7. operators
+ * 8. Functions
+ */
